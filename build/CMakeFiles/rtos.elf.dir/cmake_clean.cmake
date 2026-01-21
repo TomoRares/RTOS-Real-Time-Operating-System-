00fc1624d95b610463bb98439af344d5bf661665
Tomoiagă Rareș <tomoiaga.rares.bogdan@gmail.com>
2026-01-21 17:39:25 +0200

    Add files via upload

---
+++ build/CMakeFiles/rtos.elf.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtos.elf.dir/src/hal_gpio.c.obj"
+  "CMakeFiles/rtos.elf.dir/src/hal_gpio.c.obj.d"
+  "CMakeFiles/rtos.elf.dir/src/hal_uart.c.obj"
+  "CMakeFiles/rtos.elf.dir/src/hal_uart.c.obj.d"
+  "CMakeFiles/rtos.elf.dir/src/main.c.obj"
+  "CMakeFiles/rtos.elf.dir/src/main.c.obj.d"
+  "CMakeFiles/rtos.elf.dir/src/rtos_kernel.c.obj"
+  "CMakeFiles/rtos.elf.dir/src/rtos_kernel.c.obj.d"
+  "CMakeFiles/rtos.elf.dir/src/rtos_port.c.obj"
+  "CMakeFiles/rtos.elf.dir/src/rtos_port.c.obj.d"
+  "CMakeFiles/rtos.elf.dir/src/rtos_sync.c.obj"
+  "CMakeFiles/rtos.elf.dir/src/rtos_sync.c.obj.d"
+  "CMakeFiles/rtos.elf.dir/src/rtos_task.c.obj"
+  "CMakeFiles/rtos.elf.dir/src/rtos_task.c.obj.d"
+  "CMakeFiles/rtos.elf.dir/src/rtos_timer.c.obj"
+  "CMakeFiles/rtos.elf.dir/src/rtos_timer.c.obj.d"
+  "CMakeFiles/rtos.elf.dir/startup.c.obj"
+  "CMakeFiles/rtos.elf.dir/startup.c.obj.d"
+  "rtos.elf"
+  "rtos.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rtos.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/rtos.elf.dir/src/rtos_port.c.obj.d
@@ -0,0 +1,17 @@
+CMakeFiles/rtos.elf.dir/src/rtos_port.c.obj: \
+ /Users/tomo/Downloads/itsc/src/rtos_port.c \
+ /Users/tomo/Downloads/itsc/include/rtos.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stdint.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stddef.h \
+ /Users/tomo/Downloads/itsc/rtos_config.h \
+ /Users/tomo/Downloads/itsc/include/rtos_internal.h \
+ /Users/tomo/Downloads/itsc/include/rtos_internal.h \
+ /Users/tomo/Downloads/itsc/include/stm32f4xx.h \
+ /Users/tomo/Downloads/itsc/include/hal.h \
+ /Users/tomo/Downloads/itsc/include/stm32f4xx.h
