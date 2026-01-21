c86b8e3f122ffeff38ea37add26cbb2dbaaf94fa
Tomoiagă Rareș <tomoiaga.rares.bogdan@gmail.com>
2026-01-21 17:40:47 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/rtos.elf.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/tomo/Downloads/itsc/src/hal_gpio.c" "CMakeFiles/rtos.elf.dir/src/hal_gpio.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/src/hal_gpio.c.obj.d"
+  "/Users/tomo/Downloads/itsc/src/hal_uart.c" "CMakeFiles/rtos.elf.dir/src/hal_uart.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/src/hal_uart.c.obj.d"
+  "/Users/tomo/Downloads/itsc/src/main.c" "CMakeFiles/rtos.elf.dir/src/main.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/src/main.c.obj.d"
+  "/Users/tomo/Downloads/itsc/src/rtos_kernel.c" "CMakeFiles/rtos.elf.dir/src/rtos_kernel.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/src/rtos_kernel.c.obj.d"
+  "/Users/tomo/Downloads/itsc/src/rtos_port.c" "CMakeFiles/rtos.elf.dir/src/rtos_port.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/src/rtos_port.c.obj.d"
+  "/Users/tomo/Downloads/itsc/src/rtos_sync.c" "CMakeFiles/rtos.elf.dir/src/rtos_sync.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/src/rtos_sync.c.obj.d"
+  "/Users/tomo/Downloads/itsc/src/rtos_task.c" "CMakeFiles/rtos.elf.dir/src/rtos_task.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/src/rtos_task.c.obj.d"
+  "/Users/tomo/Downloads/itsc/src/rtos_timer.c" "CMakeFiles/rtos.elf.dir/src/rtos_timer.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/src/rtos_timer.c.obj.d"
+  "/Users/tomo/Downloads/itsc/startup.c" "CMakeFiles/rtos.elf.dir/startup.c.obj" "gcc" "CMakeFiles/rtos.elf.dir/startup.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/rtos.elf.dir/src/rtos_timer.c.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/rtos.elf.dir/src/rtos_timer.c.obj: \
+ /Users/tomo/Downloads/itsc/src/rtos_timer.c \
+ /Users/tomo/Downloads/itsc/include/rtos.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stdint.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stddef.h \
+ /Users/tomo/Downloads/itsc/rtos_config.h \
+ /Users/tomo/Downloads/itsc/include/rtos_internal.h \
+ /Users/tomo/Downloads/itsc/include/rtos_internal.h \
+ /Users/tomo/Downloads/itsc/include/stm32f4xx.h
